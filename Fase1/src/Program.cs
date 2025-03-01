bac5d04e580ee3fc0789a7c13e8c331845eda7e2
ricardious <castaneda.systems@gmail.com>
2025-02-28 22:39:32 -0600

    feat: add main application entry point

--- Fase1/src/Program.cs
+++ Fase1/src/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using AutoGestPro.src.UI.Windows;
+using Gtk;
+
+namespace AutoGestPro.src
+{
+    class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            Application.Init();
+
+            var app = new Application("org.AutoGestPro.AutoGestPro", GLib.ApplicationFlags.None);
+            app.Register(GLib.Cancellable.Current);
+
+            var win = new LoginWindow();
+            app.AddWindow(win);
+
+            win.Show();
+            Application.Run();
+        }
+    }
+}
